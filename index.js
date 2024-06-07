2be17d345103898720303f8c2c43a2406f04bfe9
IsabelaRF08 <isabelarfigueira@gmail.com>
2024-06-07 15:41:50 -0300

    Colocando arquivos no git

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const { Board, Led } = require('johnny-five')
+const express = require('express')
+
+const board = new Board()
+
+board.on('ready', () =>
+{
+    const led = new led(11)
+    const app = express()
+    app.get('/', (req, res) => 
+    {
+        res.sendFile('index.html', {root: '.'})
+
+    })
+
+    app.get('/led', (req, res) =>
+        {
+            const {turnTo} = req.query
+            if(typeof led [turnTo] == 'function')
+            {
+                led[turnTo]()
+                res.send('ligando led ${turnTo}')
+            }
+            else
+            {
+                res.status(406).send('Ação inválida')
+            }
+        })
+       
+    const port = process.env.PORT || 3000
+    app.listen(port, () => console.log(`> Servidor funcionando na porta ${port}`))
+    
+ 
+})
